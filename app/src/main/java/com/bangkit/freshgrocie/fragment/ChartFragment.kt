21d7214a372fd3bd2625e5f26e2a15b4cd61c6cf
filemon17th@gmail.com <filemon17th@gmail.com>
2023-06-05 22:24:32 +0700

    adding structur cart done

--- app/src/main/java/com/bangkit/freshgrocie/fragment/ChartFragment.kt
+++ app/src/main/java/com/bangkit/freshgrocie/fragment/ChartFragment.kt
@@ -1,11 +1,19 @@
 package com.bangkit.freshgrocie.fragment
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.GridView
+import android.widget.Toast
+import com.bangkit.freshgrocie.CartAdapter
+import com.bangkit.freshgrocie.GVStoreProductAdapter
 import com.bangkit.freshgrocie.R
+import com.bangkit.freshgrocie.viewmodel.CartModel
+import com.bangkit.freshgrocie.viewmodel.GVStoreProduct
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,12 +26,29 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ChartFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    lateinit var courseCRV: GridView
+    lateinit var handler: Handler
+    lateinit var courseList: List<CartModel>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        activity?.let {
+            toolbarTile = it.findViewById(R.id.homePage_toolbarTitle)
+        }
+        courseCRV = findViewById(R.id.wish_product)
+        courseList = ArrayList<CartModel>()
+        courseList = courseList + CartModel("C++",1500,2, R.drawable.apple_season)
+        courseList = courseList + CartModel("Java",1500,4, R.drawable.apple_season)
+        courseList = courseList + CartModel("Android",1500,5, R.drawable.apple_season)
+        courseList = courseList + CartModel("Python",1500,1, R.drawable.apple_season)
+        courseList = courseList + CartModel("Javascript",1500,3, R.drawable.apple_season)
+        val courseAdapter = CartAdapter(courseList = courseList, this@ChartFragment)
+        courseCRV.adapter = courseAdapter
+        courseCRV.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            Toast.makeText(
+                applicationContext, courseList[position].courseName + " selected",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
---
+++ app/src/main/java/com/bangkit/freshgrocie/viewmodel/CartModel.kt
@@ -0,0 +1,8 @@
+package com.bangkit.freshgrocie.viewmodel
+
+data class CartModel (
+    val courseName: String,
+    val coursePrice: Int,
+    val Qty : Int,
+    val courseImg: Int
+)
