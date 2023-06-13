f61c31aa72dd3f81aa4a140f0e1bd31868fa124f
filemon17th@gmail.com <filemon17th@gmail.com>
2023-06-14 01:07:16 +0700

    cart Response + ViewModel

--- app/src/main/java/com/bangkit/freshgrocie/database/response/ResponseCart.kt
+++ app/src/main/java/com/bangkit/freshgrocie/database/response/ResponseCart.kt
@@ -3,8 +3,6 @@ package com.bangkit.freshgrocie.database.response
 data class ResponseCart(
     val product : ResponseProductItem,
     val quantity :Int,
-//    val
 ) {
-//    constructor() : this(ResponseProductItem(), 2)
-//    constructor() :
+    constructor() : this(ResponseProductItem(), 1)
 }
